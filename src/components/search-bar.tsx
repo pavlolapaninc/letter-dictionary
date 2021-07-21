2a22ee51df6d78032797162f55645845cd62ff6d
pavlo.lapan@inclarity.co.uk <pavlo.lapan@inclarity.co.uk>
2021-07-21 09:02:36 +0300

    fix of behaviour

--- src/components/search-bar.tsx
+++ src/components/search-bar.tsx
@@ -1,29 +1,51 @@
-import React, { FunctionComponent } from 'react';
+import React, { useRef, useEffect } from 'react';
 import styled from '@emotion/styled'
 import { observer } from "mobx-react-lite"
 import store from "../store/appstate";
 
 const SearchBar = () => {
 
+  const inputEl = useRef<HTMLInputElement>(null);
+
+  useEffect(()=>{
+    if (null !== inputEl.current) {
+      inputEl.current.focus()
+    }
+  },[store.letter.letter])
+
   const text = `Enter the 'Letter' here`
 
   const Div = styled.div`
-      margin: 0 auto;
-      display: flex;
-      flex-direction: column;
-      align-items: center;`
+    margin: 0 auto;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  `
 
   const Card = styled.div`
-      margin: 15px auto;
-      padding: 10px;
-      background: #51cc6163;
-      width: 80%;
-      display: flex;
-      border: 2px solid white;
-      flex-direction: column;
-      `
+    margin: 15px auto;
+    padding: 10px;
+    background: #fff;
+    display: inline-block;
+    box-shadow: 0 0 3rem -1rem rgba(0,0,0,0.5);
+    transition: transform 0.1s ease-in-out, box-shadow 0.1s;
+    border-radius: 5px;
+    font-family: sans-serif;
+    color: #515151;
 
-    const Input = styled.input`
+    &:hover {
+      transform: translateY(-0.5rem) scale(1.0125);
+	    box-shadow: 0 0.5em 3rem -1rem rgba(0,0,0,0.5);
+    }
+  `
+
+  const Wrapper = styled.div`
+    margin: 15px auto;
+    display: flex;
+    justify-content: space-between
+  `
+
+  const Input = styled.input`
       padding: 5px 10px;
       background-color: #ff9a90;
       font-size: 24px;
@@ -42,34 +64,32 @@ const SearchBar = () => {
       }
     `
 
-  const handleChange = (e: any) =>{
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const letter = e.target.value
+    store.setNewArray(letter)
     store.findWordsByLetter(letter)
-    if(letter === "") {
-      store.setNewArray()
-      store.findWordsByLetter(letter)
-    }
   }
 
-    return(
+  return (
     <>
-        <Div>
-            <h2>{text}</h2>
-            <Input maxLength={1}
-            onChange={(e) => handleChange(e)}/>
-        </Div>
-        <Card>
-          {store.letter.firstLetter !== 0 &&
-          <p>Words starts with your letter:  {store.letter.firstLetter}</p>}
-          {store.letter.lastLetter !== 0 &&
-          <p>Words ends with your letter:  {store.letter.lastLetter}</p>}
-          {store.letter.appearing !==0 &&
-          <p>Your letter appear {store.letter.appearing} time(s)</p>}
-          {store.letter.repeating !==0 &&
-          <p>Your letter repeated in conjunction {store.letter.repeating} time(s)</p>}
-        </Card>
-      </>
-    )
+      <Div>
+        <h2>{text}</h2>
+        <Input ref={inputEl} maxLength={1}
+          onChange={(e) => handleChange(e)} 
+          value={store.letter.letter}/>
+      </Div>
+      <Wrapper>
+        {store.letter.firstLetter !== 1 &&
+          <Card>Words starts with your letter:  {store.letter.firstLetter}</Card>}
+        {store.letter.lastLetter !== 1 &&
+          <Card>Words ends with your letter:  {store.letter.lastLetter}</Card>}
+        {store.letter.appearing !== 0 &&
+          <Card>Your letter appear {store.letter.appearing} time(s)</Card>}
+        {store.letter.repeating !== 0 &&
+          <Card>Words has letter repeated in conjunction: {store.letter.repeating} time(s)</Card>}
+      </Wrapper>
+    </>
+  )
 
 };
 
--- src/store/appstate.js
+++ src/store/appstate.js
@@ -1,67 +1,60 @@
-import { observable, action, makeAutoObservable, computed, transaction } from 'mobx'
-//import dict from '../tools/dictionary'
-
+import { makeAutoObservable } from 'mobx'
 const randomPictionaryWords = require('word-pictionary-list');
 
+class AppState {
 
- class AppState {
+  letter = {
+    letter: '',
+    firstLetter: 1,
+    lastLetter: 1,
+    appearing: 0,
+    repeating: 0,
+  }
+  oldLetter = {}
+  words = []
 
-   letter =  {
-       firstLetter: 0,
-       lastLetter: 0,
-       appearing: 0,
-       repeating: 0,
-   }
-   oldLetter = {}
-   words = []
+  constructor() {
+    makeAutoObservable(this)
+  }
 
-    constructor() {
-        makeAutoObservable(this)
+
+  setWords() {
+    //Gives 10 random words from dictionary
+    this.words = randomPictionaryWords({ min: 10, max: 10 })
+  }
+
+  findWordsByLetter(letter) {
+    this.setWords()
+    this.letter.letter = letter;
+
+    this.words.map(word => {
+      /*eslint no-unused-expressions: [2, { allowTernary: true }]*/
+      letter == word.slice(0, 1) ? this.letter.firstLetter++ : null
+      letter == word.slice(-1) ? this.letter.lastLetter++ : null
+
+      const arr = word.split('')
+      for (let i = 0; i < arr.length; i++) {
+        if (arr[i] == arr[i + 1]) {
+          this.letter.repeating++
+        }
+        /*eslint no-unused-expressions: [1, { allowTernary: true }]*/
+        arr[i] == letter ? this.letter.appearing++ : null
+      }
+    })
+  }
+
+  setNewArray(letter) {
+    this.oldLetter = this.letter
+    this.letter = {
+      letter: '',
+      firstLetter: 1,
+      lastLetter: 1,
+      appearing: 0,
+      repeating: 0,
     }
 
+  }
 
-    setWords () {
-         this.words = randomPictionaryWords({ min: 3, max: 10 })
-     }
+}
 
-     findWordsByLetter(letter) {
-       this.setWords()
-
-       this.words.map(word => {
-           /*eslint no-unused-expressions: [2, { allowTernary: true }]*/
-           letter == word.slice(0,1) ? this.letter.firstLetter++ : null
-           letter == word.slice(-1) ? this.letter.lastLetter++ : null
-
-           const arr = word.split('')
-           for (let i = 0; i < arr.length; i++){
-               if(arr[i] == arr[i + 1]) {
-                   this.letter.repeating++
-               }
-               /*eslint no-unused-expressions: [1, { allowTernary: true }]*/
-               arr[i] == letter ? this.letter.appearing++ : null
-           }
-       })
-     }
-
-     setNewArray () {
-         this.oldLetter = this.letter
-         this.letter = {
-             firstLetter: 0,
-             lastLetter: 0,
-             appearing: 0,
-             repeating: 0,
-         }
-         console.log(this.letter, this.oldLetter)
-     }
-
-    // 1. How many words start with the letter <LETTER>
-    // 2. How many times does the letter <LETTER> appear in the dictionary?
-    // 3. How many words end with the letter <LETTER>?
-    // 4. How many words have the same letter repeated in conjunction?
-    // For instance, the word professor has ss, and for that, the count is 1.
-
-
-
-
- }
 export default new AppState()
